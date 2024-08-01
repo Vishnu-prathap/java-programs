c3bfe195404dc9b41a4bc53fffd12d476d60afc6
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-08-02 01:34:51 +0530

    Initial commit

---
+++ AddTwoNumbersFourFunctions.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class AddTwoNumbersFourFunctions {
+    public static int Input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number for addition:");
+        int nums = sc.nextInt();
+        return nums;
+    }
+
+    public static int Sum(int num1, int num2){
+        return num1+num2;
+    }
+    public static void  Output(int sum,int num1, int num2){
+        System.out.println("The sum of "+num1+" and "+num2+" is " + sum);
+        
+    }
+    public static void main(String[] args){
+        // Input function
+        int num1 = Input();
+        int num2 = Input();
+        // Sum function
+        int sum = Sum(num1,num2);
+        // Output function
+        Output(sum,num1,num2);
+    }
+}
