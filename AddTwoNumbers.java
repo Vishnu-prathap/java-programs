33191d2231856e409c934fd8e981f0edb0d736c9
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-08-02 00:59:33 +0530

    Simple addition of 2 integer values

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,5 +1,4 @@
 
-import java.util.Scanner;
 
 public class AddTwoNumbers{
 
@@ -8,14 +7,19 @@ public class AddTwoNumbers{
         return ans;
     }
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number");
-        int n1 = sc.nextInt();
-        System.out.println("Enter the second number");
-        int n2 = sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the first number");
+        // int n1 = sc.nextInt();
+        // System.out.println("Enter the second number");
+        // int n2 = sc.nextInt();
 
-        int sum = Sum(n1, n2);
-        // System.out.println("The sume of "+n1+" and "+n2"is"+sum);
-        System.out.println("The sum of "+n1+" and "+n2+" is "+sum);
+        // int sum = Sum(n1, n2);
+        // // System.out.println("The sume of "+n1+" and "+n2"is"+sum);
+        // System.out.println("The sum of "+n1+" and "+n2+" is "+sum);
+
+        int n1 = 10;
+        int n2 = 10;
+        int sum = n1 + n2;
+        System.out.println("The sume of "+n1+" and "+n2+" is "+sum);
     }
 }
