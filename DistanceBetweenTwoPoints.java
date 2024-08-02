06ad6123edb0673fb03c1e67faa87b7c1b34fa68
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-08-03 00:12:13 +0530

    Initial commit

---
+++ DistanceBetweenTwoPoints.java
@@ -0,0 +1,7 @@
+public class DistanceBetweenTwoPoints{
+    public static void main(String[] args){
+        // Input value of x1x2 and y1y2
+        // Calculate using distance formula
+        // Output the calculated value
+    }
+}
--- readme.txt
+++ readme.txt
@@ -1,3 +1,5 @@
 Contains the questions for the solutions coded above!
 
 1. Add two numbers with user input of the numbers with single function
+2. Distance between 2 points
+3. 
