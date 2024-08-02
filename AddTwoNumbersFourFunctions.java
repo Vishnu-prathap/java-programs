30aacf186b676a7d2ae107aa34226d85a0835216
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-08-02 23:54:26 +0530

    Removed redundant variables

--- AddTwoNumbersFourFunctions.java
+++ AddTwoNumbersFourFunctions.java
@@ -12,19 +12,17 @@ public class AddTwoNumbersFourFunctions {
     public static int Sum(int num1, int num2){
         return num1+num2;
     }
-    public static void  Output(int sum,int num1, int num2){
+    public static void  OutputSum(int sum,int num1, int num2){
         System.out.println("The sum of "+num1+" and "+num2+" is " + sum);
         
     }
     public static void main(String[] args){    
         // Input function
-        String first = "first";
-        String second = "second";
         int num1 = InputInteger("first");
         int num2 = InputInteger("second");
         // Sum function
         int sum = Sum(num1,num2);
         // Output function
-        Output(sum,num1,num2);
+        OutputSum(sum,num1,num2);
     }
 }
