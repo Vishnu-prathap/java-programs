7d4b75bf18407f2ccd2e29293ea41543f3c688ed
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-08-05 00:36:58 +0530

    Added custom output function

--- DistanceBetweenTwoPoints.java
+++ DistanceBetweenTwoPoints.java
@@ -13,7 +13,9 @@ public class DistanceBetweenTwoPoints {
     public static double DistanceCalculation(double x1, double x2, double y1, double y2) {
         return Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2));
     }
-
+    public static void DistanceBetweenTwoPointsOutput(double x1,double x2,double y1,double y2,double distanceValue){
+        System.out.println("The distance between the input points is "+distanceValue);
+    }
     public static void main(String[] args) {
         // Input value of x1x2 and y1y2
         double x1 = InputCoordinates("x1");
@@ -22,7 +24,8 @@ public class DistanceBetweenTwoPoints {
         double y2 = InputCoordinates("y2");
         // Calculate using distance formula
         double distanceValue = DistanceCalculation(x1, x2, y1, y2);
-        System.out.println(distanceValue);
+        // System.out.println(distanceValue);
         // Output the calculated value
+        DistanceBetweenTwoPointsOutput(x1, x2, y1, y2, distanceValue);
     }
 }
