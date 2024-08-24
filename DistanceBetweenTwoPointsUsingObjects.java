5e3902bbbd432e7a7bda556acaa2bab7f78d8fec
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-08-24 16:59:05 +0530

    Changed Calculate() to accept object

--- DistanceBetweenTwoPointsUsingObjects.java
+++ DistanceBetweenTwoPointsUsingObjects.java
@@ -11,7 +11,6 @@ class Point {
     public Point(double yCoordinate, double xCoordinate) {
         this.xCoordinate = xCoordinate;
         this.yCoordinate = yCoordinate;
-
     }
 
     // Multiple method to do operations
@@ -34,15 +33,22 @@ public class DistanceBetweenTwoPointsUsingObjects {
         return point;
     }
 
+    public static double CalculateDistanceObject(Point point1, Point point2) {
+        return Math.sqrt(Math.pow(point1.xCoordinate - point2.xCoordinate, 2) + Math.pow(point1.yCoordinate - point2.yCoordinate, 2));
+    }
+
     public static void main(String[] args) {
         double pointx1 = InputPoint("X1");
         double pointy1 = InputPoint("Y1");
         double pointx2 = InputPoint("X2");
         double pointy2 = InputPoint("Y2");
 
-        Point point = new Point(pointx1, pointy1, pointx2, pointy2);
-        double distance = point.CalculateDistance(pointx1, pointy1, pointx2, pointy2);
-        point.DisplayDistanceOutput(distance, pointx1, pointy1, pointx2, pointy2);
+        Point point1 = new Point(pointx1, pointy1);
+        System.out.println("point1" + point1.xCoordinate + point1.yCoordinate);
+        Point point2 = new Point(pointx2, pointy2);
+        // double distance = point1.CalculateDistance(point1.xCoordinate, point1.yCoordinate, point2.xCoordinate, point2.yCoordinate);
+        // point1.DisplayDistanceOutput(distance, pointx1, pointy1, pointx2, pointy2);
+        System.out.println("Distance" + CalculateDistanceObject(point1, point2));
 
     }
 }
