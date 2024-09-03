3437fa823f84641e1c544f92f89deca51d3d48b3
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2024-09-04 01:35:38 +0530

    Working code

--- DistanceBetweenTwoPointsUsingObjects.java
+++ DistanceBetweenTwoPointsUsingObjects.java
@@ -1,6 +1,4 @@
 
-import java.util.Dictionary;
-import java.util.Hashtable;
 import java.util.Scanner;
 
 class Point {
@@ -26,29 +24,12 @@ public class DistanceBetweenTwoPointsUsingObjects {
 
     public static Scanner sc = new Scanner(System.in);
 
-    // public static double[] InputPoint(String inputCoordinate) {
-    //     double[] arr = new double[2];
-    //     System.out.print("Enter the points " + inputCoordinate + ": ");
-    //     arr[0] = sc.nextDouble();
-    //     arr[1] = sc.nextDouble();
-    //     return arr;
-    // }
     public static Point InputPoint(String inputCoordinate) {
         System.out.print("Enter the points " + inputCoordinate + ": ");
         double xCoordinate = sc.nextDouble();
         double yCoordinate = sc.nextDouble();
-        Point pointCoordinate = new Point(xCoordinate, yCoordinate);
-        return pointCoordinate;
-    }
-
-    // @SuppressWarnings("rawtypes")
-    public static Dictionary InputDictionary(String inputCoordinates) {
-        Dictionary<String, Double> dictionary = new Hashtable<>();
-        String[] coordinateNames = inputCoordinates.split(inputCoordinates, ',');
-        System.out.print("Enter the points " + inputCoordinates + ": ");
-        dictionary.put(coordinateNames[0], sc.nextDouble());
-        dictionary.put(coordinateNames[1], sc.nextDouble());
-        return dictionary;
+        Point point = new Point(xCoordinate, yCoordinate);
+        return point;
     }
 
     public static double CalculateDistance(Point point1, Point point2) {
@@ -60,22 +41,12 @@ public class DistanceBetweenTwoPointsUsingObjects {
     }
 
     public static void main(String[] args) {
-        // double[] point1Values = InputPoint("X1,Y1");
-        // double[] point2Values = InputPoint("X2,Y2");
-        // // double pointy1 = InputPoint("Y1");
-        // double pointx2 = InputPoint("X2");2 4
-        // double pointy2 = InputPoint("Y2");
-        // Dictionary point1Dictionary = InputDictionary("X1,Y1");
-        // System.out.println(point1Dictionary);
 
         Point point1 = InputPoint("X1,Y1");
-        System.out.println(point1.xCoordinate + " " + point1.yCoordinate);
-        // Point point2 = new Point();
-        // System.out.println(point2);
-        // System.out.println("point1" + point1.xCoordinate + point1.yCoordinate);
-        // Point point2 = new Point(pointx2, pointy2);
-        // double distance = CalculateDistance(point1, point2);
-        // DisplayDistanceOutput(distance, point1, point2);
-        // System.out.println("Distance" + CalculateDistanceObject(point1, point2));
+        Point point2 = InputPoint("X2,Y2");
+        // System.out.println(point1.xCoordinate + " " + point1.yCoordinate);
+        double distance = CalculateDistance(point1, point2);
+        DisplayDistanceOutput(distance, point1, point2);
+
     }
 }
