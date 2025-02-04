39a07fb540990bc6949bc9fb532e843b3ac24283
Vishnu Prathap JRVP <85738017+Vishnu-prathap@users.noreply.github.com>
2025-02-04 23:38:51 +0530

    Codegym initial projects

---
+++ friendsAge.java
@@ -0,0 +1,7 @@
+
+public class friendsAge {
+
+    public static void main(String[] args) {
+        System.out.println(3126 - 8);
+    }
+}
---
+++ kindWordsForTeacher.java
@@ -0,0 +1,12 @@
+// package codegym;
+
+public class kindWordsForTeacher {
+
+    public static void main(String[] args) {
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+        System.out.println("Ellie is very smart");
+    }
+}
